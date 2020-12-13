ab0f9147c8db3ef89d941f4d8940b6ecf75ad5dd
TominagaKento <m153306s@st.u-gakugei.ac.jp>
2020-12-13 22:04:29 +0900

    フォルダ移動と、playgroundファイルの追加。

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,97 @@
+import UIKit
+//: ライブラリの読み込み
+//: 変数設定
+//:【大事】半角スペースをあけること！
+var str = "Hello, playground"
+
+//: 画面表示
+//:print("バッテリー残量が少なくなっています")
+print(str)
+
+//: コメントアウトの方法と、printで変数を表示させる方法
+print("文字列を表示させる方法:\(str)")
+
+//: スペースを開けないと、コメント正しく表示されない。
+print(1 == 1)
+
+//:これは、エラーになる。
+//:var word = "動物"
+//:var number = 1
+//:print(word + number)
+
+//: 変数を宣言するときに、型も一緒に宣言する方法
+var number:Int = 3
+var pi:Double = 3.14
+
+//:for文の使い方
+for x in 1...9{
+    if x % 2 == 0{
+        for n in 1...9{
+            print(x * n)
+        }
+    }
+}
+//: 変数を使って、幅指定をする場合は、以下のようにする。
+for x in 0...number{
+    print(x)
+}
+//: switch文！
+switch number{
+case 0:
+    print("0です。")
+
+case 1:
+    print("1です。")
+    
+case 2:
+    print("2です。")
+default:
+    print("0,1,2以外の値です。")
+}
+//:配列の使い方。
+var todos = ["ジョギングをする", "選択をする", "掃除をする"]
+print("\(todos[0]) その後に、\(todos[2])")
+//:foreachのような書き方ですね！
+for todo in todos{
+    print(todo)
+}
+//: 辞書（列挙型）の使い方
+var numberOfTires = ["車":4, "バイク":2, "船":0]
+print(numberOfTires["車"])
+
+//:nullではなくて、nilになる。
+numberOfTires["車"] = nil
+print(numberOfTires["車"])
+
+//:関数の使い方
+func kuku2dan(){
+    for n in 1...9{
+        print(2 * n)
+    }
+}
+
+kuku2dan()
+
+//:関数に引数を渡す。（リターンなし）
+func kuku(dan:Int){
+    for n in 1...9{
+        print(dan * n)
+    }
+}
+
+kuku(dan: 3)
+kuku(dan: 4)
+
+//: ラベルの使い方
+func areaOfTraiangle(withBase base:Int, height:Int){
+    print(base * height / 2)
+}
+areaOfTraiangle(withBase: 10, height: 3)
+
+//:関数に引数を渡す。（リターンあり）
+func areaOfTraiangle1(withBase base:Int, height:Int) -> Int{
+    let result = base * height / 2
+    return result
+}
+var area = areaOfTraiangle1(withBase: 3, height: 4)
+print(area)
